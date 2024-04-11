e1e5a9dfd20791f849e05f1d7fca680efdf71ed7
João Victor Vaz <164522321+vaz-joao@users.noreply.github.com>
2024-04-11 10:34:10 -0300

    Delete masculino_e_feminino.py

--- masculino_e_feminino.py
+++
@@ -1,8 +0,0 @@
-letra= str(input("Selecione F ou M:"))
-
-if letra == "F" or letra == "f":
- print("Feminino") 
-elif letra == "M" or letra == "m":
-  print("Masculino")
-else:
-  print("Erro, selecione F ou M")
